60961943f3adbc15f959e892f37a3309e66e9f45
YaroslavKosach <y.kosach@lampalampa.net>
2023-05-15 21:28:37 +0300

    Added button.

--- app/src/main/java/com/kosach/dictionary/MainActivity.kt
+++ app/src/main/java/com/kosach/dictionary/MainActivity.kt
@@ -1,21 +1,16 @@
 package com.kosach.dictionary
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.lifecycleScope
+import com.kosach.dictionary.databinding.ActivityMainBinding
 import com.kosach.dictionary.model.CreateWord
 import com.kosach.dictionary.use_case.CreateWordUseCase
 import com.kosach.dictionary.utils.Tags.APP_TAG
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.catch
-import kotlinx.coroutines.flow.collect
-import kotlinx.coroutines.flow.collectLatest
-import kotlinx.coroutines.flow.single
-import kotlinx.coroutines.flow.singleOrNull
 import kotlinx.coroutines.launch
 import javax.inject.Inject
-import kotlin.math.log
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
@@ -23,9 +18,12 @@ class MainActivity : AppCompatActivity() {
     @Inject
     lateinit var createWordUseCase: CreateWordUseCase
 
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         lifecycleScope.launch {
             val word = createWordUseCase(CreateWord(
@@ -38,5 +36,20 @@ class MainActivity : AppCompatActivity() {
                 "Result: ${word.id}".log(APP_TAG)
             }
         }
+
+        binding.fab.setOnClickListener {
+            lifecycleScope.launch {
+                val word = createWordUseCase(CreateWord(
+                    englishValue = "Word",
+                    ukraineValue = "Слово",
+                    description = "-"
+                )).catch {exception ->
+                    "Error".log(APP_TAG, exception)
+                }.collect {word ->
+                    "Result: ${word.id}".log(APP_TAG)
+                }
+            }
+        }
+
     }
 }
